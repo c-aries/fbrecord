abac8b716a0a9211af260c8935ba91854129af88
c-aries <babyaries2@gmail.com>
2012-01-11 16:28:39 +0800

    vc record

--- src/vcrecord.c
+++ src/vcrecord.c
@@ -4,6 +4,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/ioctl.h>
+#include <asm-generic/ioctls.h>
+#include <asm-generic/termios.h>
+#include <linux/tty.h>
 
 static void
 usage ()
@@ -13,9 +17,12 @@ usage ()
 
 int main (int argc, char *argv[])
 {
-  int fd, size;
+  int vc_fd, tty_fd, size;
   int currcons = 0;		/* current tty */
-  char ttyname[10];
+  int rows, cols, i, j;
+  char vcname[10], ttyname[10];
+  unsigned char *buf;
+  struct winsize wins;
 
   if (argc != 2) {
     usage ();
@@ -27,17 +34,42 @@ int main (int argc, char *argv[])
     fprintf (stderr, "tty number must be < 0 or > 7\n");
     exit (EXIT_FAILURE);
   }
-  snprintf (ttyname, 10, "/dev/vcs%d", currcons);
+  snprintf (vcname, 10, "/dev/vcs%d", currcons);
+  snprintf (ttyname, 10, "/dev/tty%d", currcons);
 
-  fd = open (ttyname, O_RDONLY);
-  if (fd < 0) {
+  vc_fd = open (vcname, O_RDONLY);
+  if (vc_fd < 0) {
+    fprintf (stderr, "open %s fail\n", vcname);
+    exit (EXIT_FAILURE);
+  }
+  tty_fd = open (ttyname, O_RDONLY);
+  if (tty_fd < 0) {
     fprintf (stderr, "open %s fail\n", ttyname);
+    close (vc_fd);
     exit (EXIT_FAILURE);
   }
 
-  size = lseek (fd, 0, SEEK_END);
-  printf ("size %d\n", size);
-
-  close (fd);
+  size = lseek (vc_fd, 0, SEEK_END);
+  lseek (vc_fd, 0, SEEK_SET);
+  buf = malloc (sizeof (*buf) * size);
+  read (vc_fd, buf, size);
+  if (ioctl (tty_fd, TIOCGWINSZ, &wins)) {
+    fprintf (stderr, "ioctl %s TIOCGWINSZ fail\n", ttyname);
+    free (buf);
+    close (vc_fd);
+    close (tty_fd);
+    exit (EXIT_FAILURE);
+  }
+  rows = wins.ws_row;
+  cols = wins.ws_col;
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      printf ("%c", buf[i * cols + j]);
+    }
+    puts("");
+  }
+  free (buf);
+  close (vc_fd);
+  close (tty_fd);
   exit (EXIT_SUCCESS);
 }
